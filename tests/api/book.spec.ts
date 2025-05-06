e9dd8f34870428fe693f495aed59ce2e477472db
Deepak Thomas Kuncheria <deepakt@dexlock.com>
2025-05-06 13:48:28 +0530

    inserting new book with expected success unit test added

--- tests/api/book.spec.ts
+++ tests/api/book.spec.ts
@@ -1,5 +1,5 @@
 import { db } from "@/db";
-import { users } from "@/db/schema";
+import { books, users } from "@/db/schema";
 import { test, expect } from "@playwright/test";
 import { eq } from "drizzle-orm";
 import { generateAccessToken } from "@/utils/forAuthTokens";
@@ -32,7 +32,34 @@ test.describe("Testing books api", async () => {
     const body = await res.json();
     expect(Array.isArray(body.books)).toBe(true);
   });
-  // test.describe("POST books api", () => {
-  //   test("Create a new book by author", async ({ request }) => {});
-  // });
+  test.describe("POST books api", () => {
+    let validToken: string;
+    test.beforeAll(async () => {
+      const testUserId = await db
+        .select({ id: users.id })
+        .from(users)
+        .where(eq(users.email, process.env.TEST_ADMIN as string));
+
+      validToken = generateAccessToken(testUserId[0].id);
+    });
+    test("Create a new book by author", async ({ request }) => {
+      const example = {
+        title:
+          "dummy title.............................................w232....",
+        description:
+          "lorem impsum lorem lorem impsum lorem lorem impsum loremlorem impsum loremlorem impsum loremlorem impsum loremlorem impsum loremlorem impsum lorem",
+      };
+      const res = await request.post("/api/books", {
+        headers: {
+          Authorization: `Bearer ${validToken}`,
+        },
+        data: example,
+      });
+      expect(res.status()).toBe(200);
+      const body = await res.json();
+      expect(body.data.title).toBe(example.title);
+      expect(body.data.description).toBe(example.description);
+      await db.delete(books).where(eq(books.id, body.data.id));
+    });
+  });
 });
