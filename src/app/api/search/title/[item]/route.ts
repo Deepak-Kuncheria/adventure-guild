ca0552daf3571f8a3845fd60abc95cf77399cd10
Deepak Thomas Kuncheria <deepakt@dexlock.com>
2025-06-09 18:08:27 +0530

    refactored the code

--- src/app/api/search/title/[item]/route.ts
+++ src/app/api/search/title/[item]/route.ts
@@ -1,38 +1,8 @@
 import { SERVER_ERROR } from "@/constants/errors/commonErrors";
-import { SEARCH_TITLE_LIMIT } from "@/constants/search/queryLimit";
-import { db } from "@/db";
 import { books, chapters, volumes } from "@/db/schema";
 import { checkAuthorRole } from "@/utils/authorize";
-import { matchQuery } from "@/utils/forSearch";
-import { and, desc, eq, sql } from "drizzle-orm";
+import { searchInTable } from "@/utils/forSearch";
 
-async function searchInTable(
-  tableToSearch: typeof books | typeof chapters | typeof volumes,
-  isAuthor: boolean,
-  searchItem: string
-) {
-  const whereClause =
-    isAuthor || !("isPublished" in tableToSearch)
-      ? sql`setweight(to_tsvector('english', ${
-          tableToSearch.title
-        }), 'A') @@ to_tsquery('english', ${searchItem + ":*"})`
-      : and(
-          sql`setweight(to_tsvector('english', ${
-            tableToSearch.title
-          }), 'A') @@ to_tsquery('english', ${searchItem + ":*"})`,
-          eq(tableToSearch.isPublished, true)
-        );
-  return db
-    .select({
-      id: tableToSearch.id,
-      title: tableToSearch.title,
-      rankCd: sql`ts_rank_cd(${matchQuery(tableToSearch.title, searchItem)})`,
-    })
-    .from(tableToSearch)
-    .where(whereClause)
-    .orderBy((t) => desc(t.rankCd))
-    .limit(SEARCH_TITLE_LIMIT);
-}
 export async function GET(
   req: Request,
   { params }: { params: Promise<{ item: string }> }
@@ -46,10 +16,25 @@ export async function GET(
       );
     }
     const author = await checkAuthorRole();
-
-    const bookResults = await searchInTable(books, author.status, item);
-    const volumeResults = await searchInTable(volumes, author.status, item);
-    const chapterRes = await searchInTable(chapters, author.status, item);
+    const searchLimit = 5;
+    const bookResults = await searchInTable(
+      books,
+      author.status,
+      item,
+      searchLimit
+    );
+    const volumeResults = await searchInTable(
+      volumes,
+      author.status,
+      item,
+      searchLimit
+    );
+    const chapterRes = await searchInTable(
+      chapters,
+      author.status,
+      item,
+      searchLimit
+    );
 
     return Response.json(
       {
--- src/utils/forSearch.ts
+++ src/utils/forSearch.ts
@@ -1,8 +1,39 @@
-import { sql } from "drizzle-orm";
+import { SEARCH_TITLE_LIMIT } from "@/constants/search/queryLimit";
+import { db } from "@/db";
+import { books, chapters, volumes } from "@/db/schema";
+import { and, desc, eq, sql } from "drizzle-orm";
 import { PgColumn } from "drizzle-orm/pg-core";
 
 export const matchQuery = (tableColumn: PgColumn, searchItem: string) => {
-  return sql`(
-  setweight(to_tsvector('english', ${tableColumn}), 'A')
-), to_tsquery('english', ${searchItem + ":*"})`;
+  return sql`${tableColumn}, to_tsquery('english', ${searchItem})`;
 };
+
+export async function searchInTable(
+  tableToSearch: typeof books | typeof chapters | typeof volumes,
+  isAuthor: boolean,
+  searchItem: string,
+  searchLimit: number = SEARCH_TITLE_LIMIT
+) {
+  // split the search item to create multi word prefix matching
+  const tokens = searchItem.trim().split(" ");
+  const query = tokens.map((token) => token + ":*").join("&");
+
+  const whereClause =
+    isAuthor || !("isPublished" in tableToSearch)
+      ? sql`${tableToSearch.titleSearch} @@ to_tsquery('english', ${query})`
+      : and(
+          sql`${tableToSearch.titleSearch} @@ to_tsquery('english', ${query})`,
+          eq(tableToSearch.isPublished, true)
+        );
+
+  return db
+    .select({
+      id: tableToSearch.id,
+      title: tableToSearch.title,
+      rankCd: sql`ts_rank_cd(${matchQuery(tableToSearch.titleSearch, query)})`,
+    })
+    .from(tableToSearch)
+    .where(whereClause)
+    .orderBy((t) => desc(t.rankCd))
+    .limit(searchLimit);
+}
