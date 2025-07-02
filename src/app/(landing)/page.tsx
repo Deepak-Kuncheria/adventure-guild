105ceed8d9dcab418a67c7c598cee9c8413608a0
Deepak Thomas Kuncheria <deepakt@dexlock.com>
2025-07-02 16:59:33 +0530

    UI: updated file path for better grouping

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import guildLogo from "../../public/DAG-logo.png";
-import "./page.scss";
+import guildLogo from "../../../public/DAG-logo.png";
+import "../page.scss";
 export default function Home() {
   return (
     <main>
